9a0f9eda49e340f1299149692fe4e6178d738695
Mehdi <elmahdi20erraji@gmail.com>
2024-01-12 10:52:01 +0100

    adding fiew adjustments

--- views/admin/wiki-add.php
+++ views/admin/wiki-add.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>User Add </title>
+    <title>Wiki Add </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="fully responsive." name="description" />
     <meta content="Mehdi" name="author" />
